1c75be134d56e1fb2d7e001b22571efa95802f2d
prachumchana-cell <prachumchana@icloud.com>
2026-07-21 17:22:49 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'ihobby-v1';
+const ASSETS_TO_CACHE = ['/', '/index.html'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
