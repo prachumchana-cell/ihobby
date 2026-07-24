95cdc4da94e8d0aecdba7b1adb70c30388947ebe
prachumchana-cell <prachumchana@icloud.com>
2026-07-24 14:52:02 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 const CACHE_NAME = 'ihobby-v1';
-const ASSETS_TO_CACHE = ['/', '/index.html'];
+const ASSETS_TO_CACHE =['/index.html', '/manifest.json'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
